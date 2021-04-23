ac1c1646d898d1909d3b23db016e6e633187889d
gustavo <gh.brga@gmail.com>
2021-04-23 09:18:10 -0300

    Criar listar produtos. Criar Delete categoria.

--- Services-api/src/main/java/com/example/services/Controllers/CategoriaController.java
+++ Services-api/src/main/java/com/example/services/Controllers/CategoriaController.java
@@ -1,23 +1,38 @@
 package com.example.services.Controllers;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.services.repositories.CategoriaRepository;
 
 @Controller
+@RequestMapping("/categorias")
 public class CategoriaController {
 
 	@Autowired
 	private CategoriaRepository categoria;
 	
-	@GetMapping("/categorias")
+	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView modelView = new ModelAndView("categoria/listarCategorias");
 		modelView.addObject("categorias", categoria.findAll());
 		return modelView;
+	}
+	
+	@GetMapping("delete/{id}")
+	public String deleteCustomerForm(@PathVariable Long id) {
+	    this.categoria.deleteById(id);
+	    return "redirect:/categorias";       
 	}
 	
 }
---
+++ Services-api/src/main/java/com/example/services/Controllers/ProdutoController.java
@@ -0,0 +1,25 @@
+package com.example.services.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.services.repositories.ProdutoRepository;
+
+@Controller
+@RequestMapping("/produtos")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@GetMapping
+	public ModelAndView listar() {
+		ModelAndView modelView = new ModelAndView("produto/listarProdutos");
+		modelView.addObject("produtos",  produtoRepository.findAll());
+		return modelView;
+	}
+	
+}
