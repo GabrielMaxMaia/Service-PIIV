dd85aff57fae71b1c7977fc3e26441b56ed8a8d3
gustavo <gh.brga@gmail.com>
2021-04-27 15:57:24 -0300

    Metodo criar categoria feito com sucesso!

--- Services-api/src/main/java/com/example/services/Controllers/CategoriaController.java
+++ Services-api/src/main/java/com/example/services/Controllers/CategoriaController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.services.Model.Categoria;
 import com.example.services.repositories.CategoriaRepository;
 
 @Controller
@@ -20,19 +21,27 @@ import com.example.services.repositories.CategoriaRepository;
 public class CategoriaController {
 
 	@Autowired
-	private CategoriaRepository categoria;
+	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView modelView = new ModelAndView("categoria/listarCategorias");
-		modelView.addObject("categorias", categoria.findAll());
+		modelView.addObject("categorias", categoriaRepository.findAll());
+		modelView.addObject(new Categoria());
 		return modelView;
+		
 	}
 	
 	@GetMapping("delete/{id}")
 	public String deleteCustomerForm(@PathVariable Long id) {
-	    this.categoria.deleteById(id);
+	    this.categoriaRepository.deleteById(id);
 	    return "redirect:/categorias";       
+	}
+	
+	@PostMapping
+	public String salvar(Categoria categoria) {
+		this.categoriaRepository.save(categoria);
+		return "redirect:/categorias";
 	}
 	
 }
--- Services-api/src/main/java/com/example/services/Model/Categoria.java
+++ Services-api/src/main/java/com/example/services/Model/Categoria.java
@@ -21,11 +21,11 @@ public class Categoria {
 	@NotNull
 	private String nome;
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
