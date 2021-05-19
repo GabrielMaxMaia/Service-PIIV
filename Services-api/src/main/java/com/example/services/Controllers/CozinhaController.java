fb151eac5eb4b57b4a4c3106c7cd12bc18b06e4b
leonardo-900 <leonaardo.alves@hotmail.com>
2021-05-19 20:33:49 -0300

    tela de listar pedidos da cozinha

--- Services-api/src/main/java/com/example/services/Controllers/CozinhaController.java
+++ Services-api/src/main/java/com/example/services/Controllers/CozinhaController.java
@@ -1,5 +1,7 @@
 package com.example.services.Controllers;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,27 +19,47 @@ import com.example.services.repositories.PedidoRepository;
 @Controller
 @RequestMapping("/cozinha")
 public class CozinhaController {
-	
+
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
+
 	@GetMapping("/listar")
-	public ModelAndView listar(Model model) {		 
-		
-		
-			List<String> listaPedido = cozinhaRepository.findPedidos();
-			
-			
-			System.out.println(listaPedido.get(0).toString()) ;
-			
-			//ModelAndView mv = new ModelAndView("cozinha/listarPedidos");
-			
-			//mv.addObject("pedidos", listaPedido);			
-			
-			return null;
-		
-				
-		
-		
+	public ModelAndView listar(Model model) {
+
+		List<String> listaPedido = cozinhaRepository.findPedidos();
+
+		List<String> id = new ArrayList<>();
+		List<String> nome = new ArrayList<>();
+		List<String> quantidade = new ArrayList<>();
+		List<String> observacao = new ArrayList<>();
+		List<String> status = new ArrayList<>();
+
+		for (int i = 0; i < listaPedido.size(); i++) {
+
+			String linha = " ";
+			String vLinha[] = null;
+
+			linha = listaPedido.get(i);
+
+			vLinha = linha.split(",");
+
+			id.add(vLinha[0]);
+			nome.add(vLinha[1]);
+			quantidade.add(vLinha[2]);
+			observacao.add(vLinha[3]);
+			status.add(vLinha[4]);
+
+		}
+
+		ModelAndView mv = new ModelAndView("cozinha/listarPedidos");
+
+		mv.addObject("ids", id);
+		mv.addObject("nomes", nome);
+		mv.addObject("quantidades", quantidade);
+		mv.addObject("observacoes", observacao);
+		mv.addObject("status", status);
+
+		return mv;
+
 	}
 }
