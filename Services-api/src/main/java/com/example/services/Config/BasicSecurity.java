acd790874d3c07db621f3748ba6b10dbf1c3e766
MatheusSantosDev <matheus.c.santos2@gmail.com>
2021-05-20 19:00:37 -0300

    Correções no fluxo de login

--- Services-api/src/main/java/com/example/services/Config/BasicSecurity.java
+++ Services-api/src/main/java/com/example/services/Config/BasicSecurity.java
@@ -12,11 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class BasicSecurity extends WebSecurityConfigurerAdapter{
-	
+public class BasicSecurity extends WebSecurityConfigurerAdapter {
+
 	@Autowired
-	private UserDetailsService userDetailsService; 
-	
+	private UserDetailsService userDetailsService;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
@@ -26,25 +26,34 @@ public class BasicSecurity extends WebSecurityConfigurerAdapter{
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()	
-			.anyRequest().authenticated()
-			
-			.and()
-				.formLogin().loginPage("/entrar")
-					.permitAll()
-			.and()
-				.logout().logoutSuccessUrl("/entrar?logout")
-				
-			.and()
-				.httpBasic()
-				
-			.and()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				
-			/*.and()
-				.csrf().disable()*/;
+//		http.authorizeRequests()	
+//			.anyRequest().authenticated()
+//			
+//			.and().formLogin().loginPage("/entrar").permitAll()
+//			.and()
+//				.logout().logoutSuccessUrl("/entrar?logout")
+//				
+//			.and()
+//				.httpBasic()
+//				
+//			.and()
+//				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+		/*
+		 * .and() .csrf().disable()
+		 */		
+		http.authorizeRequests()	        
+	        .anyRequest().authenticated()// Para qualquer requisição (anyRequest) é preciso estar autenticado (authenticated).
+	    .and()
+	    	.formLogin().loginPage("/entrar").permitAll()// Aqui passamos a página customizada. (permiteAll avisa o spring pra liberar o acesso pra ela)
+		.and()
+			.logout().logoutSuccessUrl("/entrar")//customização da url de logout, a url padrão causa bugs no login.
+		.and()
+			.httpBasic();		
+		
+
 	}
 }
--- Services-api/src/main/java/com/example/services/Controllers/HomeController.java
+++ Services-api/src/main/java/com/example/services/Controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.services.Controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +10,11 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		System.out.println(auth.getPrincipal()) ;
+		System.out.println(auth.getAuthorities()) ;
+		System.out.println(auth.getDetails()) ;		
+		
 		return "home"; // renderiza o arquivo templates/home.html
 	}
 	
